aed06681b2290255c4bd4933ca69df20c612d942
SARA MARJANE <saramarjane.2003@gmail.com>
2025-10-26 17:30:40 +0100

    Modifications package.json

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 console.log("Mon projet Node.js fonctionne !");
+// addition(a, b) -> retourne la somme de a et b
+function addition(a, b) {
+  return a + b; // simple addition
+}
+
+function soustraction(a, b) {
+  return a - b;
+}
+
+if (require.main === module) {
+  console.log("Résultat addition:", addition(5, 3));
+  console.log("Résultat soustraction:", soustraction(5, 3));
+}
+
+module.exports = { addition, soustraction };
+
